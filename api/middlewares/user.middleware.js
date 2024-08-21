b1c40f78abb745187b120a4dd0b8b4ef422dacd2
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-08-21 16:56:53 -0300

    feat: add email checking to user middleware

--- api/middlewares/user.middleware.js
+++ api/middlewares/user.middleware.js
@@ -8,11 +8,26 @@ const isMissingParams = (requestBody) => {
   return isBodyEmpty || !isBodyFull;
 }
 
+const isInvalidEmail = (email) => { 
+  const hasAtSign = email.indexOf("@") != -1;
+  const hasDot = email.indexOf(".") != -1;
+
+  return !hasAtSign || !hasDot;
+}
+
+
 async function checkUserSignUpParams(req, res, next) {
   if (isMissingParams(req.body)) {
     return res.status(400).send({
       error: "missing params",
       message: "request body must have username, email and password",
+    });
+  }
+
+  if (isInvalidEmail(req.body.email)) {
+    return res.status(400).send({
+      error: "invalid email",
+      message: "user email is not valid",
     });
   }
   return next();
