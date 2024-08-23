1078ecf056b1e0612c77814dfd6955b38245e366
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-08-23 15:36:56 -0300

    test: database variables non-empty

--- test/unity/database.env.test.js
+++ test/unity/database.env.test.js
@@ -1,6 +1,26 @@
 const databaseEnv = require("../../config/database.env");
 
-test('It should get none empty field at database environment variables', () => {
+
+describe("It should get none empty field at database environment variables", () => {
   
-  expect(!databaseEnv.DB_USER).toBe(false);
+  test('Database host', () => {
+    expect(!databaseEnv.DB_HOST).toBe(false);
+  });
+  
+  test('Database name', () => {
+    expect(!databaseEnv.DB_NAME).toBe(false);
+  });
+  
+  test('Database username', () => {
+    expect(!databaseEnv.DB_USER).toBe(false);
+  });
+  
+  test('Database password', () => {
+    expect(!databaseEnv.DB_PASSWORD).toBe(false);
+  });
+
+  test('Database endpoint id', () => {
+    expect(!databaseEnv.DB_ENDPOINT_ID).toBe(false);
+  });
+
 });
