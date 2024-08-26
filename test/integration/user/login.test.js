d04bd889148b8cabdbafb78b05da0845a777561f
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-08-26 10:00:43 -0300

    test: login route

---
+++ test/integration/user/login.test.js
@@ -0,0 +1,99 @@
+const getPath = require("path").resolve;
+const http = require("../requester");
+
+const { loginRoute } = require(getPath("routes/app.routes.js")).user;
+
+
+async function signUp(username, email, password) {
+  return await http.post(`/user/sign-up`)
+    .send({ username, email, password })
+}
+
+async function login(email, password) {
+  return await http
+    .post(`/user${loginRoute}`)
+    .send({ email, password });
+}
+
+
+describe("It checks login route", () => {
+
+  test("Login with success", async () => {
+    const username = `user-${Math.random()}`;
+    const email = `test-${username}@email.com`;
+    const password = "12345678";
+
+    await signUp(username, email, password);
+
+    await login(email, password).then(res => {
+      expect(res.status).toBe(200);
+    });
+
+  });
+
+  test("login with invalid email", async () => {
+    const username = `user-${Math.random()}`;
+    const email = `test-${username}@email.com`;
+    const password = "12345678";
+
+    await signUp(username, 'test@mailcom', password);
+
+    await login(email, password).then(res => {
+      console.log(res.body);
+      
+      expect(res.status).toBe(401);
+    });
+
+  });
+
+  test("Login without email", async () => {
+    const username = `user-${Math.random()}`;
+    const email = `test-${username}@email.com`;
+    const password = "12345678";
+
+    await signUp(username, '', password);
+
+    await login(email, password).then(res => {
+      expect(res.status).toBe(401);
+    });
+
+  });
+
+  test("Login with wrong password", async () => {
+    const username = `user-${Math.random()}`;
+    const email = `test-${username}@email.com`;
+    const password = "12345678";
+
+    await signUp(username, email, password);
+
+    await login(email, "asdasd5asd8").then(res => {
+      expect(res.status).toBe(401);
+    });
+
+  });
+
+  test("Login without password", async () => {
+    const username = `user-${Math.random()}`;
+    const email = `test-${username}@email.com`;
+    const password = "12345678";
+
+    await signUp(username, email, '');
+
+    await login(email, password).then(res => {
+      expect(res.status).toBe(401);
+    });
+   });
+
+  test("Login without email and password", async () => { 
+    const username = `user-${Math.random()}`;
+    const email = `test-${username}@email.com`;
+    const password = "12345678";
+
+    await signUp(username, '', '');
+
+    await login(email, password).then(res => {
+      expect(res.status).toBe(401);
+    });
+  });
+
+});
