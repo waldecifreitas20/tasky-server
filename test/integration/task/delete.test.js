bbf56a92f3a40b4936b1d93d9e988ff6b7a69bb6
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-10-10 15:12:37 -0300

    test: full tests for delete route

--- test/integration/task/delete.test.js
+++ test/integration/task/delete.test.js
@@ -5,12 +5,54 @@ describe("It checks task deleting requests possibilities", () => {
   test("Delete a task with sucess", async () => {
     const { authorization, email } = await generateUser();
 
-    /* await autoCreateTask(authorization, email);
-    const { id } = await getRandomTask(authorization);  */     
-    const { status, body } = await deleteTask(authorization, 120);
-    console.log(body);
+    await autoCreateTask(authorization, email);
+    const { id } = await getRandomTask(authorization);      
+    const { status } = await deleteTask(authorization, id);
     
     expect(status).toBe(204);
+  });
+  
+  test("Try to delete a task without token", async () => {
+    const { authorization, email } = await generateUser();
+
+    await autoCreateTask(authorization, email);
+    const { id } = await getRandomTask(authorization);      
+    const { status } = await deleteTask(null, id);
+    
+    expect(status).toBe(401);
+  });
+  
+  test("Try to delete a task that does not exist", async () => {
+    const { authorization } = await generateUser();    
+    const { status } = await deleteTask(authorization, -1);
+    
+    expect(status).toBe(404);
+  });
+  
+  test("Try to delete a task twice", async () => {
+    const { authorization, email } = await generateUser();
+
+    await autoCreateTask(authorization, email);
+    const { id } = await getRandomTask(authorization);      
+    const { status: statusA } = await deleteTask(authorization, id);
+    const { status: statusB } = await deleteTask(authorization, id);
+    
+    expect(statusA).toBe(204);
+    expect(statusB).toBe(404);
+  });
+
+  test("Try to delete a task without id", async () => {
+    const { authorization } = await generateUser();    
+    const { status } = await deleteTask(authorization, null);
+    
+    expect(status).toBe(400);
+  });
+  
+  test("Try to delete a task without id", async () => {
+    const { authorization } = await generateUser();    
+    const { status } = await deleteTask(authorization, 'asdg');
+    
+    expect(status).toBe(400);
   });
 
 });
