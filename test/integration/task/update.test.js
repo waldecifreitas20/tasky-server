b687c7b1d19c1f24d94b257c789f16a2ecf73503
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-10-24 12:32:24 -0300

    test: update task

--- test/integration/task/update.test.js
+++ test/integration/task/update.test.js
@@ -36,25 +36,178 @@ describe("It tests unexpected behavior for route that creates a new task into da
   });
 
   test("2: Update task name with success", async () => {
+    const { authorization, email } = await generateUser();
 
+    const task = {
+      name: "task",
+      date: "2020-07-01",
+      full_day: true
+    }
+
+    await createTask(authorization, {
+      user_account: email,
+      task,
+    });
+
+    task.name = "new name";
+
+    const { id } = await getRandomTask(authorization);
+    await updateTask(authorization, id, task);
+    const updatedTask = await getRandomTask(authorization);
+
+    expect(updatedTask.name).toBe(task.name);
   });
 
-  test("3: Update task description with success", async () => { });
+  test("3: Update task description with success", async () => {
+    const { authorization, email } = await generateUser();
 
-  test("4: Update task date with success", async () => { });
+    const task = {
+      name: "lalala",
+      desc: "none",
+      date: "2020-07-01",
+      full_day: true
+    }
 
-  test("5: Update task hour with success", async () => { });
+    await createTask(authorization, {
+      user_account: email,
+      task,
+    });
 
-  test("6: Set task with period of full day", async () => { });
+    task.desc = "new description";
 
-  test("7: Try to update task without send task id", async () => { });
+    const { id } = await getRandomTask(authorization);
+    await updateTask(authorization, id, task);
+    const updatedTask = await getRandomTask(authorization);
 
-  test("8: Try to update task of another user", async () => { });
+    expect(updatedTask.desc).toBe(task.desc);
+  });
 
-  test("9: Try to update task without name", async () => { });
+  test("4: Update task date with success", async () => {
+    const { authorization, email } = await generateUser();
 
-  test("10: Try to update task without date", async () => { });
+    const task = {
+      name: "lalala",
+      desc: "none",
+      date: "2020-07-01",
+      full_day: true
+    }
 
-  test("11: Try to update task without set a period", async () => { });
+    await createTask(authorization, {
+      user_account: email,
+      task,
+    });
+
+    task.date = "2023-07-01";
+
+    const { id } = await getRandomTask(authorization);
+    await updateTask(authorization, id, task);
+    const updatedTask = await getRandomTask(authorization);
+
+    expect(updatedTask.date).toBe(task.date);
+  });
+
+  test("5: Update task hour with success", async () => {
+    const { authorization, email } = await generateUser();
+
+    const task = {
+      name: "lalala",
+      desc: "none",
+      date: "2020-07-01",
+      full_day: true
+    }
+
+    await createTask(authorization, {
+      user_account: email,
+      task,
+    });
+
+    task.full_day = false;
+    task.hour = "03:45";
+
+    const { id } = await getRandomTask(authorization);
+    await updateTask(authorization, id, task);
+    const updatedTask = await getRandomTask(authorization);
+
+    expect(updatedTask.hour).toBe(task.hour);
+  });
+
+  test("6: Set task with period as full day", async () => {
+    const { authorization, email } = await generateUser();
+
+    const task = {
+      name: "lalala",
+      desc: "none",
+      date: "2020-07-01",
+      full_day: false,
+      hour: "03:45"
+    }
+
+    await createTask(authorization, {
+      user_account: email,
+      task,
+    });
+
+    task.full_day = true;
+
+    const { id } = await getRandomTask(authorization);
+    await updateTask(authorization, id, task);
+    const updatedTask = await getRandomTask(authorization);
+
+    expect(updatedTask.full_day).toBe(task.full_day);
+  });
+
+  test("7: Try to update task without send task id", async () => {
+    const { authorization, email } = await generateUser();
+
+    const task = {
+      name: "lalala",
+      desc: "none",
+      date: "2020-07-01",
+      full_day: true
+    }
+
+    await createTask(authorization, {
+      user_account: email,
+      task,
+    });
+
+    task.name = "new name";
+    task.desc = "new description";
+    task.date = "2023-07-01";
+    task.full_day = false;
+    task.hour = "03:45";
+
+    const id = null;
+    const { status } = await updateTask(authorization, id, task);
+    expect(status).toBe(400);
+  });
+
+  test("8: Try to update task of another user", async () => {
+    const userA = await generateUser();
+    const userB = await generateUser();
+
+    const task = {
+      name: "lalala",
+      desc: "none",
+      date: "2020-07-01",
+      full_day: true
+    }
+
+    await createTask(userA.authorization, {
+      user_account: userA.email,
+      task: task,
+    });
+
+    task.name = "new name";
+    task.desc = "new description";
+    task.date = "2023-07-01";
+    task.full_day = false;
+    task.hour = "03:45";
+
+    const { id } = await getRandomTask(userA.authorization);
+    const { status } = await updateTask(userB.authorization, id, task);
+
+    expect(status).toBe(404);
+  });
 
 });
